dc67047a0a12cca66b12a3e58b10767db8259b5e
Josh Howard <jashghoward@icloud.com>
2023-10-14 21:26:40 +0200

    login.js update

--- public/login.js
+++ public/login.js
@@ -1,8 +1,6 @@
 // login.js
 const loginForm = document.getElementById('login-form');
 const usernameInput = document.getElementById('username');
-
-
 const passwordInput = document.getElementById('password');
 
 loginForm.addEventListener('submit', (e) => {

4a1c98ea486a15f5470bc723531a8eb4915251b7
Josh Howard <jashghoward@icloud.com>
2023-10-14 21:24:26 +0200

    signup.html update

--- express.js
+++ express.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 
 // Middleware to parse JSON data
+
+
 app.use(bodyParser.json());
 
 // Define your routes and other middleware as needed

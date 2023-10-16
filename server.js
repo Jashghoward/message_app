a6a3df2233a7dae82a9e02e12ae77e75e010083b
Josh Howard <jashghoward@icloud.com>
2023-10-16 21:51:14 +0200

    server.js update

--- server.js
+++ server.js
@@ -128,6 +128,8 @@ app.post('/login', (req, res) => {
     if (isAuthenticated) {
         // Redirect to the message-sending page (e.g., messages.html)
         res.redirect('/messages.html');
+
+        
     } else {
         // If login fails, you can redirect to the login page with an error message
         res.redirect('/login.html?error=1');

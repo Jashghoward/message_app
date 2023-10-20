be78b41f3236531068a91b0aa68104cfb67bd139
Josh Howard <jashghoward@icloud.com>
2023-10-20 22:38:54 +0200

    signup.js update

--- public/signup.js
+++ public/signup.js
@@ -1,6 +1,8 @@
 // signup.js
 const signupForm = document.getElementById('signup-form');
 const usernameInput = document.getElementById('username');
+
+
 const passwordInput = document.getElementById('password');
 
 signupForm.addEventListener('submit', (e) => {
@@ -9,7 +11,7 @@ signupForm.addEventListener('submit', (e) => {
     // Get the entered username and password
 
 
-    
+
 
     const username = usernameInput.value;
     const password = passwordInput.value;
